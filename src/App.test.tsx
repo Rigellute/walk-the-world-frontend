b078193ba68f1ba2692c8eaf39673077c4a6bb2f
Rigellute <rigellute@gmail.com>
2019-12-30 10:29:59 +0000

    Add test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Test that the Nav title is there", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/OVPA Walk the World/i);
   expect(linkElement).toBeInTheDocument();
 });
